e035607942acb7a71691158479760118dc1054c9
Saidmirzo <saidmirzobahromov88@gmail.com>
2023-01-04 18:48:25 +0500

    first commit

---
+++ lib/presentation/routes/routes.dart
@@ -0,0 +1,31 @@
+import 'package:e_commerce/presentation/pages/main/view/main_page.dart';
+import 'package:flutter/material.dart';
+
+
+class Routes {
+  static const signPage = '/signPage';
+  static const signCodePage = '/signCodePage';
+  static const landingPage = '/';
+  static const tvPage = '/tvPage';
+
+  static Route<dynamic> generateRoute(RouteSettings routeSettings) {
+    try {
+      final Map<String, dynamic>? args =
+          routeSettings.arguments as Map<String, dynamic>?;
+      args ?? <String, dynamic>{};
+      switch (routeSettings.name) {
+        
+        default:
+          return MaterialPageRoute(
+            settings: routeSettings,
+            builder: (_) => const MainPage(),
+          );
+      }
+    } catch (e) {
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const MainPage(),
+      );
+    }
+  }
+}
