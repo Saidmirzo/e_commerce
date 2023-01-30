f33921be15b5835ec6dce3745a80babe3f9b1358
Javoh29 <33982593+Javoh29@users.noreply.github.com>
2023-01-30 23:01:27 +0500

    ok

---
+++ lib/presentation/pages/thanks/view/thanks_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThanksPage extends StatelessWidget {
+  const ThanksPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
